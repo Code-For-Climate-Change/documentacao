fefa24bff7599e27ba4cd4f7cc8129690beaacd5
Beatriz Seraphim <beatrizoseraphim@gmail.com>
2023-04-18 14:13:00 -0300

    Atualização da Documentação

--- postagens_blogpessoal.sql
+++
@@ -1,8 +0,0 @@
-USE db_blogpessoal;
-
-INSERT INTO tb_postagens (data, texto, titulo) 
-VALUES (current_timestamp(), 'Texto da postagem 01', 'Postagem 01');
-INSERT INTO tb_postagens (data, texto, titulo) 
-VALUES (current_timestamp(), 'Texto da postagem 02', 'Postagem 02');
-
-SELECT * FROM tb_postagens;
